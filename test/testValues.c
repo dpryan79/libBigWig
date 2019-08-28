d8e93e700afd088117ba0a39a81f61de4aec80d4
Brent Pedersen <bpederse@gmail.com>
2019-08-28 08:48:00 -0600

    more tests

--- test/testValues.c
+++ test/testValues.c
@@ -49,6 +49,11 @@ int main(int argc, char *argv[]) {
     uint32_t stops[] =  {101, 3044, 40068, 50000};
     float values[] = {10.1, 100.1, 1000.1, 5000.1};
     bwAddIntervals(ofp, chroms, starts, stops, values, 4);
+
+    bwAddIntervalSpanSteps(ofp, "1", 60000, 100, 100, values, 4);
+    starts[0] = 70000; starts[1] = 70001; starts[2] = 70002; starts[3] = 70003;
+    bwAddIntervalSpans(ofp, "1", starts, 1, values, 4);
+
     bwClose(ofp);
 
 
@@ -57,28 +62,45 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "An error occured while opening for reading %s\n", argv[2]);
         return 1;
     }
+    int L = 100000;
 
-    float *output = malloc(sizeof(float)*50000);
+    float *output = malloc(sizeof(float)*L);
 
-    bwGetOverlappingValues(ifp, "1", 0, 50000, 0.0, output);
+    bwGetOverlappingValues(ifp, "1", 0, L, 0.0, output);
     int i;
     for(i=0; i < 100;i++){
-        if(!almost_equal(output[i], 0.0)) { fprintf(stderr, "bad missing value at output[%d]: %.1f\n", i, output[i]); }
+        if(!almost_equal(output[i], 0.0)) { fprintf(stdout, "bad missing value at output[%d]: %.1f\n", i, output[i]); }
     }
     for(i=100; i < 101;i++){
-        if(!almost_equal(output[i], 10.1)) { fprintf(stderr, "bad float value at output[%d]: %.1f\n", i, output[i]); }
+        if(!almost_equal(output[i], 10.1)) { fprintf(stdout, "bad float value at output[%d]: %.1f\n", i, output[i]); }
     }
     for(i=3000; i < 3044;i++){
-        if(!almost_equal(output[i], 100.1)) { fprintf(stderr, "bad float value at output[%d]: %.1f\n", i, output[i]); }
+        if(!almost_equal(output[i], 100.1)) { fprintf(stdout, "bad float value at output[%d]: %.1f\n", i, output[i]); }
     }
     for(i=40004; i < 40068;i++){
-        if(!almost_equal(output[i], 1000.1)) { fprintf(stderr, "bad float value at output[%d]: %.1f\n", i, output[i]); }
+        if(!almost_equal(output[i], 1000.1)) { fprintf(stdout, "bad float value at output[%d]: %.1f\n", i, output[i]); }
     }
     for(i=49001; i < 50000;i++){
-        if(!almost_equal(output[i], 5000.1)) { fprintf(stderr, "bad float value at output[%d]: %.1f\n", i, output[i]); }
+        if(!almost_equal(output[i], 5000.1)) { fprintf(stdout, "bad float value at output[%d]: %.1f\n", i, output[i]); }
     }
-
-    //fprintf(stderr, "output[3011] should be 100.1 %.1f\n", output[3011]);
+    // span steps
+    for(i=60000; i < 60100;i++){
+        if(!almost_equal(output[i], 10.1)) { fprintf(stdout, "span step: bad float value at output[%d]: %.1f\n", i, output[i]); }
+    }
+    for(i=60100; i < 60200;i++){
+        if(!almost_equal(output[i], 100.1)) { fprintf(stdout, "span step: bad float value at output[%d]: %.1f\n", i, output[i]); }
+    }
+    for(i=60200; i < 60300;i++){
+        if(!almost_equal(output[i], 1000.1)) { fprintf(stdout, "span step: bad float value at output[%d]: %.1f\n", i, output[i]); }
+    }
+    for(i=60300; i < 60400;i++){
+        if(!almost_equal(output[i], 5000.1)) { fprintf(stdout, "span step: bad float value at output[%d]: %.1f\n", i, output[i]); }
+    }
+    // spans
+    if(!almost_equal(output[70000], 10.1)) { fprintf(stdout, "span: bad float value at output[%d]: %.1f\n", 7000, output[7000]); }
+    if(!almost_equal(output[70001], 100.1)) { fprintf(stdout, "span: bad float value at output[%d]: %.1f\n", 7001, output[7001]); }
+    if(!almost_equal(output[70002], 1000.1)) { fprintf(stdout, "span: bad float value at output[%d]: %.1f\n", 7002, output[7002]); }
+    if(!almost_equal(output[70003], 5000.1)) { fprintf(stdout, "span: bad float value at output[%d]: %.1f\n", 7003, output[7003]); }
 
     free(output);
     bwClose(ifp);
@@ -88,7 +110,7 @@ int main(int argc, char *argv[]) {
     return 0;
 
 error:
-    fprintf(stderr, "Got an error somewhere!\n");
+    fprintf(stdout, "Got an error somewhere!\n");
     bwClose(ifp);
     bwClose(ofp);
     bwCleanup();
